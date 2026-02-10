71f0736bdb75dac1b7b7e8b0bc376d04530a3cdc
Siddharth <sidmaneo6@gmail.com>
2026-02-10 22:02:30 +0530

    Initial frontend and backend setup with root gitignore

---
+++ frontend/src/App.tsx
@@ -0,0 +1,9 @@
+import './App.css'
+
+export default function App() {
+  return (
+    <div className="min-h-screen flex items-center justify-center bg-black text-white">
+      Tailwind is working 🚀
+    </div>
+  )
+}
